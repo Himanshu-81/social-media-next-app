f3331704c53ecfd3f2a3c71607285ac4e47189bd
Himanshu <hc200108@gmail.com>
2025-08-08 00:40:24 +0530

    refactor: improve login function error handling and user validation logic

--- src/app/(auth)/login/actions.ts
+++ src/app/(auth)/login/actions.ts
@@ -9,56 +9,55 @@ import { lucia } from "@/auth";
 import { cookies } from "next/headers";
 
 export async function login(
-    credentials: LoginValues
+  credentials: LoginValues
 ): Promise<{ error: string }> {
-    try {
-        const { username, password } = loginSchema.parse(credentials);
+  try {
+    const { username, password } = loginSchema.parse(credentials);
 
-        const existingUser = await prisma.user.findFirst({
-            where: {
-                username: {
-                    equals: username,
-                    mode: "insensitive"
-                }
-            }
-        })
+    const existingUser = await prisma.user.findFirst({
+      where: {
+        username: {
+          equals: username,
+          mode: "insensitive",
+        },
+      },
+    });
 
-
-        if (!existingUser || existingUser.password) {
-            return {
-                error: "Invalid username or password."
-            };
-        }
-
-        const validPassword = await verify(
-            existingUser.passwordHash, password, {
-            memoryCost: 19456,
-            timeCost: 2,
-            parallelism: 1,
-            outputLen: 32
-        }
-        )
-
-        if (!validPassword) {
-            return {
-                error: "Invalid username or password."
-            };
-        }
-
-
-        const session = await lucia.createSession(existingUser.id, {})
-        const sessionCookie = lucia.createSessionCookie(session.id);
-
-        (await cookies()).set(sessionCookie.name, sessionCookie.value, sessionCookie.attributes);
-
-        return redirect("/")
-
-    } catch (error) {
-        if (isRedirectError(error)) throw error;
-        console.error(error);
-
-        return {
-            error: "Something went wrong. Please try again later."
-        };
+    if (!existingUser || existingUser.googleId || !existingUser.passwordHash) {
+      return {
+        error: "Invalid username or password.",
+      };
     }
+
+    const validPassword = await verify(existingUser.passwordHash, password, {
+      memoryCost: 19456,
+      timeCost: 2,
+      parallelism: 1,
+      outputLen: 32,
+    });
+
+    if (!validPassword) {
+      return {
+        error: "Invalid username or password.",
+      };
+    }
+
+    const session = await lucia.createSession(existingUser.id, {});
+    const sessionCookie = lucia.createSessionCookie(session.id);
+
+    (await cookies()).set(
+      sessionCookie.name,
+      sessionCookie.value,
+      sessionCookie.attributes
+    );
+
+    return redirect("/");
+  } catch (error) {
+    if (isRedirectError(error)) throw error;
+    console.error(error);
+
+    return {
+      error: "Something went wrong. Please try again later.",
+    };
+  }
 }
