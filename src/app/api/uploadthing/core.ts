89b4f195207d50d772251e18b0802f05b9706da4
Himanshu <hc200108@gmail.com>
2025-08-17 23:54:07 +0530

    feat: add upload functionality with avatar handling and user validation

---
+++ src/app/api/uploadthing/core.ts
@@ -0,0 +1,40 @@
+import { validateRequest } from "@/auth";
+import prisma from "@/lib/prisma";
+import { createUploadthing, FileRouter } from "uploadthing/next";
+import { UploadThingError } from "uploadthing/server";
+
+const f = createUploadthing();
+
+export const uploadThingFileRouter = {
+  avatar: f({
+    image: { maxFileSize: "512KB" },
+  })
+    .middleware(async () => {
+      const { user } = await validateRequest();
+
+      if (!user) throw new UploadThingError("Unauthorized");
+
+      return { user };
+    })
+
+    // metadata contains the user return above
+    .onUploadComplete(async ({ metadata, file }) => {
+      const newAvatarUrl = file.url.replace(
+        "/f/",
+        `/a/${process.env.NEXT_PUBLIC_UPLOADTHING_APP_ID}/`
+      );
+
+      await prisma.user.update({
+        where: {
+          id: metadata.user.id,
+        },
+        data: {
+          avatarUrl: newAvatarUrl,
+        },
+      });
+
+      return { avatarUrl: newAvatarUrl };
+    }),
+} satisfies FileRouter;
+
+export type UploadThingFileRouter = typeof uploadThingFileRouter;
