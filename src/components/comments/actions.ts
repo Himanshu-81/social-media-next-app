7a0cf755f62fec2f8bf957b300b48af31cf12bfd
Himanshu <hc200108@gmail.com>
2025-09-19 01:10:32 +0530

    feat: add comments functionality with API endpoints and validation

---
+++ src/components/comments/actions.ts
@@ -0,0 +1,31 @@
+"use server";
+
+import { validateRequest } from "@/auth";
+import prisma from "@/lib/prisma";
+import { getCommentDataSelect, PostData } from "@/lib/types";
+import { createCommentSchema } from "@/lib/validation";
+
+export async function submitComment({
+  post,
+  content,
+}: {
+  post: PostData;
+  content: string;
+}) {
+  const { user } = await validateRequest();
+
+  if (!user) throw Error("Unauthorized");
+
+  const { content: contentValidated } = createCommentSchema.parse({ content });
+
+  const newComment = await prisma.comment.create({
+    data: {
+      content: contentValidated,
+      postId: post.id,
+      userId: user.id,
+    },
+    include: getCommentDataSelect(user.id),
+  });
+
+  return newComment;
+}
